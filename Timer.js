a5ce7df268e32231db59282979d0dcf0b9065634
kthurman216 <33873133+kthurman216@users.noreply.github.com>
2022-09-05 21:02:01 -0500

    working timer and counter
    Word counter and goal settings functional, timer functional

---
+++ Timer.js
@@ -0,0 +1,98 @@
+export default class Timer {
+    constructor(root) {
+        root.innerHTML = Timer.getHTML();
+
+        this.elem = {
+            minutes: root.querySelector(".timer-minutes"),
+            seconds: root.querySelector(".timer-seconds"),
+            controls: root.querySelector(".timer-control"),
+            reset: root.querySelector(".timer-reset")
+        };
+
+        this.interval = null;
+        this.remainingSeconds = 0;
+
+
+        this.elem.controls.addEventListener("click", () => {
+            if (this.interval === null) {
+                this.start();
+            }
+            else {
+                this.stop();
+            }
+        });
+
+        this.elem.reset.addEventListener("click", () => {
+            const inputTime = prompt("Enter number of minutes:");
+
+            if (inputTime < 60) {
+                this.stop();
+                this.remainingSeconds = inputTime * 60;
+                this.updateInterfaceTime();
+            }
+        });
+    }
+
+    updateInterfaceTime() {
+        const minutes = Math.floor(this.remainingSeconds / 60);
+        const seconds = this.remainingSeconds % 60;
+
+        this.elem.minutes.textContent = minutes.toString().padStart(2, "0");
+        this.elem.seconds.textContent = seconds.toString().padStart(2, "0");
+    }
+
+    updateInterfaceControls() {
+        if (this.interval === null) {
+            this.elem.controls.innerHTML = `<span class="material-icons">play_arrow</span>`;
+            this.elem.controls.classList.add("timer-start");
+            this.elem.controls.classList.remove("timer-stop");
+        }
+        else {
+            this.elem.controls.innerHTML = `<span class="material-icons">pause</span>`;
+            this.elem.controls.classList.add("timer-stop");
+            this.elem.controls.classList.remove("timer-start");
+        }
+    }
+
+    start() {
+        if (this.remainingSeconds === 0) {
+            return;
+        }
+
+        this.interval = setInterval(() => {
+            this.remainingSeconds--;
+            this.updateInterfaceTime();
+
+            if (this.remainingSeconds === 0) {
+                this.stop();
+                document.getElementById("alert").play();
+            }
+        }, 1000);
+
+        this.updateInterfaceControls();
+    }
+
+    stop() {
+        clearInterval(this.interval);
+        this.interval = null;
+
+        this.updateInterfaceControls();
+    }
+
+    static getHTML() {
+        return `
+            <span id="timer-icon" class="timer-part">
+                <i class="material-icons">timer</i>
+            </span>
+            <span class="timer-part timer-minutes">00</span>
+            <span class="timer-part">:</span>
+            <span class="timer-part timer-seconds">00</span>
+            <button class="timer-button timer-control timer-start">
+                <i class="material-icons">play_arrow</i>
+            </button>
+            <button class="timer-button timer-reset">
+                <i class="material-icons">access_time</i>
+            </button>
+        `;
+    }
+}
--- script.js
+++ script.js
@@ -1,92 +1,40 @@
-let formatButtons = document.querySelectorAll(".format-button");
-let advancedFormatOptions = document.querySelectorAll(".adv-format-opt");
-let fontName = document.getElementById("font-name");
-let fontSizeRef = document.getElementById("font-size");
+import Timer from "./Timer.js";
+
+    new Timer(document.querySelector(".timer"));
+
+/* WORD COUNTER/WORD GOAL FUNCTIONALITY */
 let textArea = document.getElementById("text-input");
-let alignButtons = document.querySelectorAll(".align"); //text align buttons, e.g. Center, Right, Left, Justify
-let indentOptions = document.querySelectorAll(".indent"); //increase/decrease indent buttons
-let styleButtons = document.querySelectorAll(".format"); //bold, italic, underline, strikthrough, etc
-let scriptButtons = document.querySelectorAll(".script"); //subscript, superscript
-let listButtons = document.querySelectorAll(".list"); //ordered list, unordered list
 
-//font list
-let fontList = ["Arial", "Verdana", "Tahoma", "Trebuchet MS", "Times New Roman", "Georgia", "Garamond", 
-"Courier New", "Brush Script MT"];
+let wordCount = document.getElementById("word-count");
+let wordGoal = document.getElementById("word-goal");
+let colorChangeable = document.querySelectorAll(".color-changeable");
 
-//initialize settings
-const initializer = () => {
-    //function calls to set up highlight for buttons that use it
-    highlighter(alignButtons, true);
-    highlighter(styleButtons, false);
-    highlighter(scriptButtons, true);
-    highlighter(indentOptions, true);
-    highlighter(listButtons, true);
+textArea.addEventListener("input", () => {
+    let txt = textArea.innerText.trim();
 
-    //adds font from font list to font selector
-    fontList.map(value => {
-        let option = document.createElement("option");
-        option.value = value;
-        option.innerHTML = value;
-        fontName.appendChild(option);
-    });
+    //word counter
+    wordCount.textContent = txt.split(/\s+|[,.?!]/).filter((item) => item).length;
 
-    //adds font sizes to size selector
-    for (let i= 1; i<= 7; i++) {
-        let option = document.createElement("option");
-        option.value = i;
-        option.innerHTML = i;
-        fontSizeRef.appendChild(option);
+    if (parseInt(wordCount.innerText) >= parseInt(wordGoal.innerText)) {
+        colorChangeable.forEach((item) => {
+            item.classList.add("achieved");
+        });
+    }
+    if (parseInt(wordCount.innerText) < parseInt(wordGoal.innerText)) {
+        colorChangeable.forEach((item) => {
+            item.classList.remove("achieved");
+        });
+    }
+});
+
+wordGoal.addEventListener("click", () => {
+    wordGoal.innerText = "";
+});
+
+wordGoal.addEventListener("blur", () => {
+    const containsNonDigit = /\D+/.test(wordGoal.innerText);
+    if (containsNonDigit) {
+        wordGoal.innerText = "0";
     }
 
-    //set default font size
-    fontSizeRef.value = 3;
-};
-
-
-const modifyText = (command, defaultUI, value) => {
-    document.execCommand(command, defaultUI, value);
-};
-
-formatButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        modifyText(button.id, false, null);
-    });
 });
-
-advancedFormatOptions.forEach((button) => {
-    button.addEventListener("change", () => {
-        modifyText(button.id, false, button.value);
-    });
-});
-
-
-//highlight buttons
-const highlighter = (className, needsRemoval) => {
-    className.forEach((button) => {
-        button.addEventListener("click", () => {
-            if(needsRemoval) {
-                let alreadyActive = false;
-
-                if (button.classList.contains("active")) {
-                    alreadyActive = true;
-                }
-
-                highlightRemover(className);
-                if (!alreadyActive) {
-                    button.classList.add("active");
-                }
-            }
-            else {
-                button.classList.toggle("active");
-            }
-        });
-    });
-};
-
-const highlightRemover = (className) => {
-    className.forEach((button) => {
-        button.classList.remove("active");
-    });
-}
-
-window.onload = initializer();
